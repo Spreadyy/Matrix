1e5724f3476874ff0e81eaaa62bea18d28280d17
Spreadyy <sandro@012345.ch>
2016-11-15 20:30:29 +0100

    Add project files.

---
+++ Matrix/Matrix.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Matrix
+{
+    public struct Matrix
+    {
+        private readonly double[,] Values;
+
+        public Matrix(double[,] values)
+        {
+            if (values == null)
+            {
+                throw new ArgumentNullException("values may not be null");
+            }
+            Values = values;
+        }
+
+        public string[,] GetStringValues()
+        {
+            var stringValues = new string[RowCount(),ColumnCount()];
+            for (int row = 0; row < RowCount(); row++)
+            {
+                for (int column = 0; column < ColumnCount(); column++)
+                {
+                    stringValues[row, column] = Values[row, column].ToString();
+                }
+            }
+            return stringValues;
+        }
+
+        public static Matrix operator +(Matrix left, Matrix right)
+        {
+            if (left.RowCount() != right.RowCount() ||
+                left.ColumnCount() != right.ColumnCount())
+            {
+                throw new ArgumentException("To calculate the sum of two matrixes they need to have the same row and column count.");
+            }
+
+            var newValues = new double[left.RowCount(), left.ColumnCount()];
+            for (int row = 0; row < left.RowCount(); row++)
+            {
+                for (int column = 0; column < left.ColumnCount(); column++)
+                {
+                    newValues[row, column] = left.Values[row, column] + right.Values[row, column];
+                }
+            }
+
+            return new Matrix(newValues);
+        }
+
+        public static Matrix operator *(Matrix left, Matrix right)
+        {
+            if (left.ColumnCount() != right.RowCount())
+            {
+                throw new ArgumentException("To calculate the product of two matrixes the left matrix needs to have as many columns as the right matrix has rows.");
+            }
+
+            var newValues = new double[left.RowCount(), right.ColumnCount()];
+            for (int leftRow = 0; leftRow < left.RowCount(); leftRow++)
+            {
+                for (int rightCol = 0; rightCol < right.ColumnCount(); rightCol++)
+                {
+                    double sum = 0;
+                    for (int i = 0; i < left.ColumnCount(); i++)
+                    {
+                        sum += left.Values[leftRow, i] * right.Values[i, rightCol];
+                    }
+
+                    newValues[leftRow, rightCol] = sum;
+                }
+            }
+
+            return new Matrix(newValues);
+        }
+
+        public int RowCount()
+        {
+            return Values.GetLength(0);
+        }
+
+        public int ColumnCount()
+        {
+            return Values.GetLength(1);
+        }
+    }
+}
---
+++ Matrix/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using ConsoleTables.Core;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix m1 = new Matrix(new double[,]{
+                { 3.14, 2.41 },
+                { 1.56, 21.4 }
+            });
+            Matrix m2 = new Matrix(new double[,]{
+                { 23.1, 4.13 },
+                { 8.52, 41.4 }
+            });
+
+            PrintTable(m1.GetStringValues());
+            PrintTable(m2.GetStringValues());
+            PrintTable((m1 + m2).GetStringValues());
+            PrintTable((m1 * m2).GetStringValues());
+            Console.ReadLine();
+        }
+
+        static void PrintTable(string[,] table)
+        {
+            ConsoleTable consoleTable = new ConsoleTable();
+            for (int row = 0; row < table.GetLength(0); row++)
+            {
+                int rowLength = table.GetLength(1);
+                var rowArr = new string[rowLength];
+                for (int column = 0; column < rowLength; column++)
+                {
+                    rowArr[column] = table[row, column];
+                }
+                if (row == 0)
+                {
+                    consoleTable.AddColumn(rowArr);
+                }
+                else
+                {
+                    consoleTable.AddRow(rowArr);
+                }
+            }
+            consoleTable.Write(Format.Alternative);
+        }
+    }
+}
